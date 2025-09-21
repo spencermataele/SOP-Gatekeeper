88a34b14e96542a08f75ebed4753f68599661194
spencermataele <spencermataele@gmail.com>
2025-09-21 12:04:13 -0400

    Add sop.service details in UI

--- frontend/src/app/features/sops/services/sop.service.ts
+++ frontend/src/app/features/sops/services/sop.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Sop} from "../models/sop.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SopService {
+  private base = `${environment.apiBaseUrl}/sops`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  list(): Observable<Sop[]> {
+    return this.http.get<Sop[]>(this.base);
+  }
+  get(id: number): Observable<Sop> {
+    return this.http.get<Sop>(`${this.base}/${id}`);
+  }
+  create(body: Sop): Observable<Sop> {
+    return this.http.post<Sop>(this.base, body);
+  }
+  update(id: number, body: Sop): Observable<Sop> {
+    return this.http.put<Sop>(`${this.base}/${id}`, body);
+  }
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.base}/${id}`);
+  }
 }
