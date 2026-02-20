62d056300adae758de711dffc47b78703a57f267
spencermataele <spencermataele@gmail.com>
2026-02-19 23:18:44 -0500

    Updates frontend environment base url to railway url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8080/api'
+  apiBaseUrl: 'https://woven1-frontend-production.up.railway.app'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080'
+  apiBaseUrl: 'https://woven1-frontend-production.up.railway.app'
 };
 
 /*
